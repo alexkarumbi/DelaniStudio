8be9e1fe12848228d280a17b01b0f0da92c426fb
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-07-25 18:42:23 +0300

    add slide up and down to what we do

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#destxt").click(function(){
+        $("#desp").toggle(1000);
+    })
+    $("#devtxt").click(function(){
+        $("#devp").toggle(1000);
+    })
+    $("#protext").click(function(){
+        $("#prop").toggle(1000);
+    })
+})
