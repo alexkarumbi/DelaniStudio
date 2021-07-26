b9003afd987b98323ed90b2a2ffb816f8b48ab2e
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-07-26 08:06:23 +0300

    add form validation

--- JS/index.js
+++ JS/index.js
@@ -8,4 +8,25 @@ $(document).ready(function() {
     $("#protext").click(function(){
         $("#prop").toggle(1000);
     })
+
+    $(".image-overlay").hover(function(){
+
+        $(".image-title").css("border", "1px solid","white");
+
+        $(".image-title").css("border-radius","2px")
+        $(".image-title").css("padding","5px")
+    });
+
 })
+
+var validation =function (){
+    let name =document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+
+    if(name==="" || email===""){
+        alert("Enter details")
+    }else{
+        alert("Your message has been received")
+    }
+
+}
