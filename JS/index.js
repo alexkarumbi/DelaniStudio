36855cb5bd5d5d1b6d3721f57e2ec7d2073ea93f
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-07-26 08:44:15 +0300

    add styling to portfolio

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,39 @@
-$(document).ready(function() {
-    $("#destxt").click(function(){
-        $("#desp").toggle(1000);
-    })
-    $("#devtxt").click(function(){
-        $("#devp").toggle(1000);
-    })
-    $("#protext").click(function(){
-        $("#prop").toggle(1000);
-    })
+$(document).ready(function () {
+  $("#destxt").click(function () {
+    $("#desp").toggle(1000);
+  });
+  $("#devtxt").click(function () {
+    $("#devp").toggle(1000);
+  });
+  $("#protext").click(function () {
+    $("#prop").toggle(1000);
+  });
 
-    $(".image-overlay").hover(function(){
+  $(".image-overlay").hover(function () {
+    $(".image-title").css("border", "1px solid", "white");
 
-        $(".image-title").css("border", "1px solid","white");
+    $(".image-title").css("border-radius", "2px");
+    $(".image-title").css("padding", "5px");
+  });
+  $(document).ready(function () {
+    $(".text").hover(
+      function () {
+        $(this).animate({ opacity: "1" });
+      },
+      function () {
+        $(this).animate({ opacity: "0" });
+      }
+    );
+  });
+});
 
-        $(".image-title").css("border-radius","2px")
-        $(".image-title").css("padding","5px")
-    });
-    $(document).ready(function(){
-        $('.text').hover(function () {
-        $(this).animate({opacity:'1'});
-        },
-        function () {
-        $(this).animate({opacity:'0'});
-    })
-    });
+var validation = function () {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
 
-})
-
-var validation =function (){
-    let name =document.getElementById("name").value;
-    let email=document.getElementById("email").value;
-
-    if(name==="" || email===""){
-        alert("Enter details")
-    }else{
-        alert("Your message has been received")
-    }
-
-}
+  if (name === "" || email === "") {
+    alert("Enter details");
+  } else {
+    alert("Your message has been received");
+  }
+};
